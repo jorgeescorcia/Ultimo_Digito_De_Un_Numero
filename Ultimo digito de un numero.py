d0db9dc36f3570bd5bff72396ac8f7da15775dbd
Jorge Andres Escorcia <44179214+jorgeescorcia@users.noreply.github.com>
2020-09-21 20:14:36 -0500

    Create Ultimo digito de un numero.py

---
+++ Ultimo digito de un numero.py
@@ -0,0 +1,6 @@
+#Escribe el código para leer un número entero e imprime el último dígito.
+def reto3():
+    numero = input("Ingresa el numero: ")
+    print("El ultimo digito del numero es: ", numero[-1])
+
+reto3()
